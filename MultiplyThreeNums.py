fb4c926ac051fe69261a18f3ac0549384ef29949
xxdexx233 <1813692108@qq.com>
2026-03-23 21:47:45 +0800

    multiply three numbers

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,11 @@
+#Multiply three numbers
+
+#user input
+a = float(input("Please enter the first number:"))
+b = float(input("Please enter the second number:"))
+c = float(input("Please enter the third number:"))
+
+#output
+res = float(a*b*c)
+print("The result is")
+print(res)
